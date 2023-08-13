ce513db6af3e141f0cd4c402cdc5edfc6aa18609
Rajesh Verma <119964171+RajeshVerma1612@users.noreply.github.com>
2023-08-13 11:43:40 +0000

    Conversion Operation is Done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+// let score=33 //o/p=number  
+// let score="33" //o/p=number 
+// let score="33abc" //o/p=number 
+// let score="rajesh" //o/p=number
+// let score=null //o/p=number 
+// let score=undefined //o/p=number 
+// let score=true //o/p=number 
+
+
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let value=Number(score)
+// console.log(value)
+ 
+/*
+33=33
+"33"=33
+"33abc"=NaN 
+"rajesh"=NaN 
+null=0
+udefined=NaN
+true/false=1/0
+*/
+
+// console.log(typeof value)
+
+
+
+let isLoggedIn=1
+let check=Boolean(isLoggedIn)
+console.log(check)
+/*
+1=true / -1=true
+0=false
+""=false
+"abc"=true
+undefined=false
+null=false
+*/
+console.log(typeof check)
