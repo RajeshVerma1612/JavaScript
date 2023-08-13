9dabcae97e177d784a781cc122ddf51d1cc9a2cf
Rajesh Verma <119964171+RajeshVerma1612@users.noreply.github.com>
2023-08-13 11:00:38 +0000

    variables is done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=12322
+let accountEmail="Rajesh"
+var password="12345"
+account ="Raniganj"
+
+
+// accountId=2
+accountEmail="Rahul"
+password=223313
+account="Asansol"
+
+console.log(typeof accountId)
+console.table([accountId,accountEmail,password,account])
+ 
---
+++ 01_Basics/test.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+
+const userName="Rajesh";
+let age=22;
+var password=1234567;
+ 
+str1="rajesh"
+str2=" verma";
+str3=str1+str2;
+console.log(+"");
